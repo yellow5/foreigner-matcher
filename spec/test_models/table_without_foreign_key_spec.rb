aec6dd83a5c4f8ca00dc7880de7f52e97500c633
Cameron Dykes <yellow5c@gmail.com>
2011-04-12 09:23:00 -0500

    Use single quotes where possible for consistency

--- spec/test_models/table_without_foreign_key_spec.rb
+++ spec/test_models/table_without_foreign_key_spec.rb
@@ -4,16 +4,16 @@ describe TableWithoutForeignKey do
   describe 'messages' do
     let(:matcher) { have_foreign_key_for(:users) }
 
-    it "should contain a description" do
-      matcher.description.should == "have a foreign key for users"
+    it 'should contain a description' do
+      matcher.description.should == 'have a foreign key for users'
     end
 
-    it "should set failure_message_for_should message" do
+    it 'should set failure_message_for_should message' do
       matcher.matches?(subject)
       matcher.failure_message_for_should.should == "expected  to include #{Foreigner::ConnectionAdapters::ForeignKeyDefinition.new('table_without_foreign_keys', 'users', :primary_key => 'id', :column => 'user_id', :name => 'table_without_foreign_keys_user_id_fk')}"
     end
 
-    it "should set failure_message_for_should_not message" do
+    it 'should set failure_message_for_should_not message' do
       matcher.matches?(subject)
       matcher.failure_message_for_should_not.should == "expected  to exclude #{Foreigner::ConnectionAdapters::ForeignKeyDefinition.new('table_without_foreign_keys', 'users', :primary_key => 'id', :column => 'user_id', :name => 'table_without_foreign_keys_user_id_fk')}"
     end
