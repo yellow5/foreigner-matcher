e1d05f445f554f41b2bc45403b4089a11f8de967
Cameron Dykes <yellow5c@gmail.com>
2012-02-18 00:10:31 -0600

    Add helper to retrieve foreign keys for the received table

--- spec/support/connection_helpers.rb
+++ spec/support/connection_helpers.rb
@@ -1,4 +1,8 @@
 module ConnectionHelpers
+  def foreign_keys_for(table_name)
+    ActiveRecord::Base.connection.foreign_keys(table_name)
+  end
+
   def postgresql_db?
     ActiveRecord::Base.connection.adapter_name.downcase == 'postgresql'
   end
